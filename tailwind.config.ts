aa3a6a61335489c7f2fb29fdc7500b71f304e5ff
Nenad <nbursa@gmail.com>
2024-06-03 12:10:23 +0200

    Extend tailwind.config.ts colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          gray: '#587291',
+          blue: '#2F97C1',
+          light: '#1CCAD8',
+          lighter: '#15E6CD',
+          green: '#0CF574',
+        }
+      }
+    }
   },
   variants: {
     extend: {},
